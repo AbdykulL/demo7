0f85eba37f8ddaf6baf9eb8c267f9f4b8fc13aa9
AbdykulL <lailaabdikul@gmail.com>
2025-05-13 23:38:02 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector('.burger').addEventListener('click',function(){
+
+    document.querySelector('.nav').classList.toggle('active');
+    document.querySelector('.burger').classList.toggle('burger-active');
+
+});
+
